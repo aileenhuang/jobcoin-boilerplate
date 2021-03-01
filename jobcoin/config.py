2a91ce3d9e2d90ee7ec726a83f4ac5572ab56b96
Aileen Huang <yeeruhuang@gmail.com>
2021-02-28 19:50:51 -0500

    Update API url in config.py

--- jobcoin/config.py
+++ jobcoin/config.py
@@ -3,6 +3,6 @@ sys.exit('\n!!! Please update config.py with your personal Jobcoin URL and '
          'remove this message.\n')
 
 # Replace the URL below
-API_BASE_URL = 'https://jobcoin.projecticeland.net/changeme/api'
+API_BASE_URL = 'https://jobcoin.gemini.com/todo_my_instance/api'
 API_ADDRESS_URL = '{}/addresses'.format(API_BASE_URL)
 API_TRANSACTIONS_URL = '{}/transactions'.format(API_BASE_URL)
